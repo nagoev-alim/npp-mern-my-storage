e46414144aa862e7bde3712de24ad1df4ada904d
Alim Nagoev <nagoev@MacBook-Pro-Alim.local>
2023-03-23 18:42:22 +0300

    Add vercel.json

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,9 @@ const app = express();
 // app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
+app.use(cors({
+  credential: '*',
+}));
 
 // Подключение маршрутов
 app.use('/api', router);
